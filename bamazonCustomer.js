a23cae50fa487a6a2589af508582ae03164a9892
Martel Ramdin <martel1983@yahoo.com>
2018-06-04 16:40:33 -0500

    working check qty calling updateTable

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-var mysql = require("mysql");
-var inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id", connection.threadId + "\n");
     start();
@@ -17,29 +17,70 @@ connection.connect(function(err) {
 
 /*Created a start fucntion that pulls in all the items based on quantity > 0*/
 function start() {
-    connection.query("SELECT * FROM products WHERE stock_quantity > 0 ORDER BY department_name;", function(err, results) {
+    console.log("Hello, Welcome to Bamazon!");
+    console.log("*********************************************************");
+    connection.query("SELECT * FROM products WHERE stock_quantity > 0 ORDER BY department_name;", function (err, results) {
         if (err) throw err;
-    for (let i = 0; i< results.length; i++) {
-        console.log("Product_ID:",results[i].item_id," ","Product_Name:",results[i].product_name," "," "," Price: ",results[i].price);
-    }
-});
+        for (let i = 0; i < results.length; i++) {
+            console.log("Product_ID:", results[i].item_id, " ", "Product_Name:", results[i].product_name, " ", " ", " Price: ", results[i].price);
+        }
+        console.log("*********************************************************");
+        console.log("");
+        setTimeout(prompter, 2000);
+    });
 }
 
- function prompter() {
-    inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "rawlist",
-        message: "Would you like to [POST] an auction or [BID] on an //auction?",
-        choices: ["POST", "BID"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid.toUpperCase() === "POST") {
-          postAuction();
-        }
-        else {
-          bidAuction();
-        }
-      });
 
+
+
+
+/*The app should then prompt users with two messages.
+
+   * The first should ask them the ID of the product they would like to buy.
+   * The second message should ask how many units of the product they would like to buy.*/
+
+function prompter() {
+    inquirer
+        .prompt([{
+                name: "id",
+                type: "input",
+                message: "Please enter ID(number) of product you would like to purchase:"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "Please enter how many units(quantity) to purchase?"
+            }
+        ]).then(function (answer) {
+            var qty = parseFloat(answer.quantity);
+            var id = parseFloat(answer.id);
+            // based on their answer, checks to see if NAN or not then sends to quantity-
+            if (!isNaN(qty) && !isNaN(id)) {
+                checkQTY(qty, id);
+            } else {
+                console.log("Please enter numeric value only! Re-enter!");
+                prompter();
+            }
+        });
+}
+//Check if quantity is avaiable. Make query call by item_id to see if any results have 0 quantity. 
+function checkQTY(qty, id) {
+
+    connection.query("SELECT * FROM products WHERE stock_quantity = 0 ORDER BY department_name;", function (err, results) {
+        if (err) throw err;
+        for (let i = 0; i < results.length; i++) {
+            if (id == results[i].item_id) {
+                let a = results[i].item_id;
+                console.log("**+**We are out-of-stock today, sorry try again.**+**");
+                prompter();
+                break;
+            } 
+                else {
+                updateTable();
+            }
+        }
+    });
+}
+    function updateTable() {
+        console.log("hello");
+    }
--- commands.sql
+++ commands.sql
@@ -2,6 +2,6 @@ SELECT * FROM bamazondb.products LIMIT 1000;
 
 SELECT * FROM products WHERE product_name = "bowl";
 
-SELECT * FROM products WHERE stock_quantity > 0 ORDER BY department_name;
+SELECT * FROM products WHERE stock_quantity = 0 ORDER BY department_name;
 
  
