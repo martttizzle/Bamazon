bc57623a045c88617a613c4b26df655329c013a8
Martel Ramdin <martel1983@yahoo.com>
2018-06-05 14:18:52 -0500

    got math to work and the total bill; adjusted display with console logs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,21 +17,19 @@ connection.connect(function (err) {
 
 /*Created a start fucntion that pulls in all the items based on quantity > 0*/
 function start() {
-    console.log("Hello, Welcome to Bamazon!");
-    console.log("*********************************************************");
-    connection.query("SELECT * FROM products WHERE stock_quantity > 0 ORDER BY department_name;", function (err, results) {
+        console.log("******************************-Hello, Welcome to Bamazon!-***********************************");
+    connection.query("SELECT * FROM products WHERE stock_quantity >= 0 ORDER BY department_name;", function (err, results) {
         if (err) throw err;
         for (let i = 0; i < results.length; i++) {
-            console.log("Product_ID:", results[i].item_id, " ", "Product_Name:", results[i].product_name, " ", " ", " Price: ", results[i].price);
+            console.log("_________________________________________________________________________________________");
+            console.log("ID#:",results[i].item_id,"\n", "\tproduct name:", results[i].product_name,"\t","price:", results[i].price,"\t","quantity:", results[i].stock_quantity);
+           
         }
-        console.log("*********************************************************");
         console.log("");
-        setTimeout(prompter, 2000);
+        console.log("*********************************************************************************************");
+        setTimeout(prompter, 1500);
     });
 }
-
-
-
 
 
 /*The app should then prompt users with two messages.
@@ -70,19 +68,52 @@ function checkQTY(qty, id) {
         if (err) throw err;
         let qtyYes = false;
         for (let i = 0; i < results.length; i++) {
-            if (id == results[i].item_id) {  
+            if (id == results[i].item_id) {
                 qtyYes = true;
                 console.log("*********We are out-of-stock today, sorry try again in a few days!.*********");
                 prompter();
                 break;
-            }  
+            }
         }
-        if(qtyYes === false) {
-            updateTable();
+        if (qtyYes === false) {
+            qtyMath(qty, id);
         }
-        
     });
 }
-    function updateTable() {
-        console.log("hello");
-    }
+//Calculates the Math for qty and price and sends results to updateTable function
+function qtyMath(qty, id) {
+    var tst = connection.query(
+        "SELECT * FROM products WHERE ?", [{
+            item_id: id
+        }],
+        function (err, res) {
+        
+            let qtyLeft = (res[0].stock_quantity - qty);
+            let total = (res[0].price * qty);
+            if (qtyLeft < 0) {
+                console.log("***quantity exceeds stock amount, please try again!***");
+                start();
+            }else{
+            updateTable(qtyLeft, id, total);
+            }
+        });
+}
+//updataTable takes in the values from qtyMath and updates table
+function updateTable(qty, id, total) {
+    total = total.toFixed(2);
+    console.log("Updating, one moment please!...\n");
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?", [{
+                stock_quantity: qty
+            },
+            {
+                item_id: id
+            }
+        ],
+        function (err, res) {
+            console.log(res.affectedRows + " products updated!\n");
+            console.log("***$",total, "Total Sale***");  
+            setTimeout(start, 5000);
+        }
+    );
+}
