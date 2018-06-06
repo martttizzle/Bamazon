0c8b04fd05c293d319c31a411f51fc719bae6c86
Martel Ramdin <martel1983@yahoo.com>
2018-06-06 16:16:42 -0500

    Major changes to fully functional, still have to make it idiot-proof

--- BamazonManger.js
+++ BamazonManger.js
@@ -2,6 +2,7 @@
 const mysql = require("mysql");
 const inquirer = require('inquirer');
 
+var stockQuantity = 0;
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,7 +14,8 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id", connection.threadId + "\n");
-    start();
+    // start();
+    prompter();
 });
 
 /*Created a start function that pulls in all the items based on quantity > 0 */
@@ -33,82 +35,192 @@ function start() {
 }
 /*The app should then prompt users with two messages. Then depending on the response from user, it checks on the quanity first then move to function */
 function prompter() {
-    inquirer
-        .prompt([{
-                name: "id",
-                type: "input",
-                message: "Please enter ID# of product you would like to purchase:"
-            },
-            {
-                name: "quantity",
-                type: "input",
-                message: "Please enter how many units(quantity) to purchase?"
-            }
-        ]).then(function (answer) {
-            var qty = parseFloat(answer.quantity);
-            var id = parseFloat(answer.id);
-            // based on their answer, checks to see if NAN or not then sends to quantity-
-            if (!isNaN(qty) && !isNaN(id)) {
-                checkQTY(qty, id);
-            } else {
-                console.log("Please enter numeric value only! Re-enter!");
-                prompter();
-            }
-        });
-}
-//Check if quantity is avaiable. Make query call by item_id to see if any results have 0 quantity. 
-//If the items are in stock then call math function.  
-function checkQTY(qty, id) {
-    connection.query("SELECT * FROM products WHERE stock_quantity = 0 ORDER BY department_name;", function (err, results) {
-        if (err) throw err;
-        let qtyYes = false;
-        for (let i = 0; i < results.length; i++) {
-            if (id == results[i].item_id) {
-                qtyYes = true;
-                console.log("*********We are out-of-stock today, sorry try again in a few days!.*********");
-                prompter();
+    inquirer.prompt([{
+        type: "list",
+        name: "userInput",
+        message: "Choose from available choices below:",
+        choices: ["View Products", "View Low-Inventory", "Add to Inventory", "Add New Products"]
+    }]).then(function (answer) {
+        console.log(answer.userInput, "choice");
+        switch (answer.userInput) {
+            case "View Products":
+                listItems();
                 break;
-            }
-        }
-        if (qtyYes === false) {
-            qtyMath(qty, id);
+            case "View Low-Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                addQty();
+                break;
+            case "Add New Products":
+                addItem();
+                break;
+            default:
+                "fail"
         }
     });
 }
-//Calculates the Math for qty and price and sends results to updateTable function. if the user enters 
-//more than quantity available it start back(recursion)
-function qtyMath(qty, id) {
-    var tst = connection.query(
-        "SELECT * FROM products WHERE ?", [{
+//Check if quantity is avaiable. Make query call by item_id to see if any results have 0 quantity. 
+//If the items are in stock then call math function.  
+function listItems() {
+    connection.query("SELECT * FROM products;", function (err, results) {
+        if (err) throw err;
+        console.log("***********************************INVENTORY******************************************");
+        for (let i = 0; i < results.length; i++) {
+            console.log("_________________________________________________________________________________________");
+            console.log("ID#:", results[i].item_id, "\n", "\tproduct name:", results[i].product_name, "\t", "price:",
+                results[i].price, "\t", "quantity:", results[i].stock_quantity);
+        }
+        console.log("");
+        console.log("*********************************************************************************************");
+        inquirer.prompt([{
+            type: "list",
+            name: "userInput",
+            message: "Choose from available choices below:",
+            choices: ["<--Back"]
+        }]).then(function (answer) {
+            if (answer.userInput) {
+                prompter();
+            }
+        })
+    });
+}
+function addQty() {
+    inquirer.prompt([{
+        type: "list",
+        message: "Choose Department",
+        name: "qtyDept",
+        choices: ["Clothing", "Electronics", "Food", "Kitchen", "Sporting Goods"]
+
+    }]).then(function (answer) {
+        let deptN = answer.qtyDept;
+        connection.query("SELECT * FROM products WHERE ?", [{
+                department_name: deptN
+            }],
+            function (err, results) {
+                if (err) throw err;
+                console.log("**********************************_INVENTORY_******************************************");
+                for (let i = 0; i < results.length; i++) {
+                    console.log("_________________________________________________________________________________________");
+                    console.log("ID#:", results[i].item_id, "\n", "\tproduct name:", results[i].product_name, "\t",
+                        "quantity:", results[i].stock_quantity);
+                }
+                prodID(results);
+            });
+    });
+}
+
+function prodID(results) {
+    console.log('');
+    inquirer.prompt([{
+        type: "input",
+        message: "Choose Product ID# to add to stock:",
+        name: "prdID"
+    }, {
+        type: "input",
+        message: "How many unit(s) to add?",
+        name: "prdUnits"
+    }]).then(function (answer) {
+        for(var i =0; i< results.length; i++) {
+            if(results[i].item_id == answer.prdID) {
+                let units = results[i].stock_quantity;
+                let sumQuantity = (units + parseFloat(answer.prdUnits));
+                let id = parseFloat(answer.prdID);
+                updatingStock(sumQuantity, id);
+                break;
+            }
+        }
+    })
+}
+
+function updatingStock(sum, id) {
+    connection.query("UPDATE products SET ? WHERE ?", 
+        [{
+            stock_quantity: sum
+        },{
             item_id: id
         }],
-        function (err, res) {
-            let qtyLeft = (res[0].stock_quantity - qty);
-            let total = (res[0].price * qty);
-            if (qtyLeft < 0) {
-                console.log("***quantity exceeds stock available, please try again!***");
-                start();
-            } else {
-                updateTable(qtyLeft, id, total);
-            }
-        });
+        function (err, results) {
+            if (err) throw err;
+            console.log("--Adjustment was successful!--")
+            prompter();
+        })
 }
-//updateTable takes in the values from qtyMath and updates table
-function updateTable(qty, id, total) {
-    total = total.toFixed(2);
-    var query = connection.query(
-        "UPDATE products SET ? WHERE ?", [{
-                stock_quantity: qty
-            },
-            {
-                item_id: id
-            }
-        ],
-        //prints out total sale then after 5 seconds starts back Bamazon.
-        function (err, res) {
-            console.log("Updating, one moment please!...\n");
-            console.log("***$", total, "Total Sale***");
-            setTimeout(start, 5000);
+
+
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5 ORDER BY department_name", function (err, results) {
+        if (err) throw err;
+        console.log("***********************************LOW-INVENTORY******************************************");
+        for (let i = 0; i < results.length; i++) {
+            console.log("_________________________________________________________________________________________");
+            console.log("ID#:", results[i].item_id, "\n", "\tproduct name:", results[i].product_name, "\t", "price:",
+                results[i].price, "\t", "quantity:", results[i].stock_quantity);
         }
-    );
+        console.log("");
+        console.log("*********************************************************************************************");
+        inquirer.prompt([{
+            type: "list",
+            name: "userInput",
+            message: "Choose from available choices below:",
+            choices: ["<--Back"]
+        }]).then(function (answer) {
+            if (answer.userInput) {
+                prompter();
+            }
+        })
+    })
 }
+
+
+//add new product
+function addItem() {
+
+    inquirer.prompt([
+     {
+        type: "input",
+        name: "pName",
+        message: "New product name?"
+
+     },{
+         type: "input",
+         name: "dName",
+         message: "Department name?"
+
+     },{ 
+         type: "input",
+         name: "price",
+         message: "Price of item?"
+
+     },{
+        type: "input",
+        name: "qty",
+        message: "How many to add?"  
+    }]).then(function (answer) {
+        let prodName = answer.pName;
+        let deptName = answer.dName;
+        let priceAdd = answer.price;
+        let qtyAdd = answer.qty;
+        insertNewItem(prodName, deptName, priceAdd, qtyAdd);
+    })
+ }
+
+ function insertNewItem(prodName, deptName, priceAdd, qtyAdd){
+     connection.query(
+      "INSERT INTO products SET ?",
+      {
+        product_name: prodName,
+        department_name: deptName,
+        price: priceAdd, 
+       stock_quantity: qtyAdd
+      }
+    ); 
+    console.log("______________________________");
+    console.log("--Item was successfully added--");
+    console.log("______________________________");
+    console.log("Returning to main menu in 3 seconds");
+    setTimeout(prompter, 3000);
+}  
+ 
+
+
